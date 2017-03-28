2b5ae1524f19838d2555a2554a2637bd4e67c9d8
leandrouspBR <leandroasilva290@gmail.com>
2017-03-28 13:06:48 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 int main()
 {
     system("color 2");
-    printf("C L O C K\n\n");
+    printf("A L A R M E\n\n");
+
 
     int sec=0, min=0, hour=0;
 
@@ -33,9 +34,44 @@ int main()
     }
     printf("\n\n");
 
+
+    int alarme_sec=0, alarme_min=0, alarme_hour=0;
+
+    printf("Horario do alarme"); //le o horario do alarme
+    printf("\nHora:");
+    scanf("%d", &alarme_hour);
+    while(alarme_hour<0||alarme_hour>=24)
+    {
+        printf("Invalido, digite novamente:");
+        scanf("%d", &alarme_hour);
+    }
+    printf("Minuto:");
+    scanf("%d", &alarme_min);
+    while(alarme_min<0||alarme_min>=60)
+    {
+        printf("Invalido, digite novamente:");
+        scanf("%d", &alarme_min);
+    }
+    printf("Segundo:");
+    scanf("%d", &alarme_sec);
+     while(alarme_sec<0||alarme_sec>=60)
+    {
+        printf("Invalido, digite novamente:");
+        scanf("%d", &alarme_sec);
+    }
+    printf("\n\n");
+
     while(1)
     {
         printf("\r Horario: %2dh %2dmin %2ds", hour, min, sec);
+
+        if((hour==alarme_hour)&&(min==alarme_min)&&(sec==alarme_sec))
+        {
+            printf("\n *****A L A R M E******\n\n");
+            puts("\a");
+            break;
+        }
+
         Sleep(1000);
         sec++;
 
